011c9f838eeaf0efe1e1cee36a2668518f6e5fc7
rbaker4559 <reginaldsbaker10@icloud.com>
2024-01-16 22:14:27 -0800

    created prompts and generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,21 @@
+/* 
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+*/
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,8 +25,19 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+};
 
-}
+//generates password
+function generatePassword() {
+  //
+  var characterCount = prompt ("Length of password (min 8 characters; max 128 characters");
+  var inclLower = prompt ("Contain lowercase characters? (Y/N)");
+  var inclUpper = prompt ("Contain uppercase characters? (Y/N)");
+  var inclNumeric = prompt ("Contain numeric characters? (Y/N)");
+  var inclSpecial = prompt ("Contain special characters? (Y/N)");
+};
+
+generatePassword();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
